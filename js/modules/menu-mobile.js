6a6bac9e57e5d7c24342cbe833a64f4880036ad3
pauloesmelos <email>
2022-10-30 01:43:01 -0300

    added menu mobile and anime numbers

---
+++ js/modules/menu-mobile.js
@@ -0,0 +1,37 @@
+export default function initMenuMobile(){
+
+}
+let button_open = document.querySelector('.button');
+let ul_menu = document.querySelector('.ul-nav');
+let nav = document.querySelector('.menu-nav');
+let html = document.documentElement;
+let events = ['click','touchstart'];
+let botaoAtivo = 'data-ativo';
+
+events.forEach((e) => button_open.addEventListener(e,openmenu));
+function openmenu(){
+    this.nextElementSibling.classList.toggle('ativo');//ou ul_menu.classList.add('ativo');
+    initClickoutside(this);
+    //!ul_menu.classList.contains('ativo') ? this.removeAttribute(botaoAtivo) : this.setAttribute(botaoAtivo,'');
+    if(!ul_menu.classList.contains('ativo')){
+        this.removeAttribute(botaoAtivo);
+       // events.forEach((e) => button_open.removeEventListener(e,openmenu));
+    }
+    else
+        this.setAttribute(botaoAtivo,'');
+}
+
+function initClickoutside(elemento){
+   if(!elemento.hasAttribute(botaoAtivo) && !elemento.hasAttribute('aux')){
+        events.forEach(e => html.addEventListener(e,outsideclick));
+        function outsideclick(event){ 
+            if(!nav.contains(event.target)){
+                ul_menu.classList.remove('ativo');
+                events.forEach((e) => ul_menu.removeEventListener(e,outsideclick));//SEMPRE VERIFICAR SE O EVENTO É ACUMULADO E REMOVER!
+                elemento.removeAttribute(botaoAtivo);
+                elemento.removeAttribute('aux');
+            }
+        }
+        elemento.setAttribute('aux','');
+   }
+}
---
+++ js/modules/numeros-animados.js
@@ -0,0 +1,37 @@
+export default function initAnimateNumbers(){
+
+}
+const text = document.querySelectorAll('[data-anima="numero"]');
+function animatenumbers(){
+    if(text.length){
+        text.forEach((e) => {
+            const toNumber = +e.innerText;
+            let contador = 0;
+
+            let interval = setInterval(() => {
+                contador += Math.ceil(toNumber / 100);
+                e.innerText = contador;
+
+                if(contador > toNumber){
+                    clearInterval(interval);
+                    e.innerText = toNumber;
+                }
+            },100 * Math.random());//anima um elemento num tempo diferente do outro
+        });
+    }
+}
+function callback(mutations){
+    mutations.forEach((e) => {
+        //console.log(e.target.classList.contains('showsection'));
+        if(e.target.classList.contains('showsection')){
+            animatenumbers();
+            observador.disconnect();
+        }
+    });
+}
+let sectionalvo = document.getElementById('numeros');
+let observador = new MutationObserver(callback);
+observador.observe(sectionalvo,{
+    attributes: true,
+    attributeOldValue: true,
+});
